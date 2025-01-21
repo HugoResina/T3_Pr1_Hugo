ab6053a7b696b6802af660035918f195dcf7fbf4
HugoResina <hugo.resina.7e7@itb.cat>
2025-01-21 10:30:27 +0100

    strucutre defined

---
+++ T3_Pr1_Hugo/SistemaHidroelectric.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3_Pr1_Hugo
+{
+    internal class SistemaHidroelectric : ASistemaEnergia
+    {
+        public SistemaHidroelectric(double EnergiaGenerada, string TipusEnergia) : base(EnergiaGenerada, TipusEnergia)
+        {
+        }
+
+        public override double CalculEnergia()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ConfiguracioParametres()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ T3_Pr1_Hugo/SistemaSolar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3_Pr1_Hugo
+{
+    public class SistemaSolar : ASistemaEnergia
+    {
+
+        public SistemaSolar(double EnergiaGenerada, string TipusEnergia) : base(EnergiaGenerada, TipusEnergia)
+        {
+        }
+
+        public override double CalculEnergia()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ConfiguracioParametres()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
